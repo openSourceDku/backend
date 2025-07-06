ce1f15c6a2c85995322716163251bb45f7488752
maruiNia <maruinano@gmail.com>
2025-07-06 13:04:20 +0900

    add logout modify send_test.py

--- apps/acounts/urls.py
+++ apps/acounts/urls.py
@@ -1,11 +1,12 @@
 # user/urls.py
 from django.urls import path
-from .views import RegisterView, CustomTokenObtainPairView
+from .views import RegisterView, CustomTokenObtainPairView, LogoutView
 from rest_framework_simplejwt.views import TokenRefreshView
 
 app_name = "acounts"
 urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register'),
-    path('token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),  # 여기만 바뀜
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('register/', RegisterView.as_view(), name='register'),                     #계정 등록
+    path('login/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'), 
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
--- send_test.py
+++ send_test.py
@@ -13,8 +13,8 @@ def register() :
     print("응답 상태", response.status_code)
     print("응답 상태", response.text)
 
-def token() :
-    url = "http://127.0.0.1:8000/acounts/token/"
+def login() :
+    url = "http://127.0.0.1:8000/acounts/login/"
     data = {
         "username": "kimgeon",
         "password": "1234!",
@@ -24,6 +24,34 @@ def token() :
     response = requests.post(url, json=data)
     print("응답 상태", response.status_code)
     print("응답 상태", response.text)
-    print(response.json())
 
-token()
+def refresh() :
+    url = "http://127.0.0.1:8000/acounts/refresh/"
+    data = {
+        "refresh": 'yJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoicmVmcmVzaCIsImV4cCI6MTc1MjM3ODEzMCwiaWF0IjoxNzUxNzczMzMwLCJqdGkiOiJhOTQ4ODk5ZTE0Njg0YWM0YTFiZDA2ZTExZjA1MjAxOSIsInVzZXJfaWQiOjN9.zcEUZUB36FE8YrNetZl-sSc8dFrWbaUpvjIV-f5mzO0'
+    }
+
+    response = requests.post(url, json=data)
+    print("응답 상태", response.status_code)
+    print("응답 상태", response.text)
+
+def logout() :
+    url = "http://127.0.0.1:8000/acounts/logout/"
+
+    refresh = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoicmVmcmVzaCIsImV4cCI6MTc1MjM3OTE1NywiaWF0IjoxNzUxNzc0MzU3LCJqdGkiOiJjMzA5MmQzNGE5NTE0Njc5YWQ0OTljOGUzYzFjOTNhNiIsInVzZXJfaWQiOjN9.QChxhEcKl7im85lP5iBlsWliTw07p-aLsCmlZdvvkNI"
+    access = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzUxNzc2MTU3LCJpYXQiOjE3NTE3NzQzNTcsImp0aSI6IjFiOWM4NWYzMTlkNzQ0YmZiNjc4MTEzNzk1OWFjZjFiIiwidXNlcl9pZCI6M30.VGVjTTBVo6-uk3wf4Xp84_NgGqYdmNxjXbROTilcQ8g"
+    
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {access}"
+    }
+
+    data = {
+        "refresh": refresh
+    }
+
+    response = requests.post(url, headers=headers, json=data)
+    print("응답 상태", response.status_code)
+    print("응답 상태", response.text)
+
+logout()
