220278d444f4f3319c95ac51e9f9d36d6d511400
maruiNia <maruinano@gmail.com>
2025-07-06 14:13:23 +0900

    teachers 모델 수정

--- apps/teachers/models.py
+++ apps/teachers/models.py
@@ -1,10 +1,15 @@
 from django.db import models
+from apps.acounts.models import CustomUser
 
 # Create your models here.
 
 class Teacher(models.Model) :
-    teacher_id = models.IntegerField(primary_key=True)
-    passwd = models.CharField(max_length=100)
+    '''
+    상위 모델인 CustomUser에 만들어진 username으로 teacher_id를 검색한다.
+    Teacher의 teacher_id은 CustomUser의 username를 외래키로 가지며, 서로를 유일하게 식별한다.
+    password, role은 CustomUser에 등록되어 있다.
+    '''
+    teacher_id = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     age = models.IntegerField()
     name = models.CharField(max_length=100)
     position = models.CharField(max_length=100)
