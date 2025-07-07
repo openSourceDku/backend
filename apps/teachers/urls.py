9dfc53a2ddfcc080904dbe5d54bd72b76d7a882c
maruiNia <maruinano@gmail.com>
2025-07-07 22:24:36 +0900

    url업데이트

--- apps/teachers/urls.py
+++ apps/teachers/urls.py
@@ -10,5 +10,9 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
     # path('', views.teachers_api, name='teachers_api'),
     # path('classes/<int:class_id>', views.get_class_students, name='get_class_students'),
-    path("reports/", views.ReportSendView.as_view, name="reports")
+    path("reports/", views.ReportSendView.as_view, name="reports"),
+    # path("reports/", views.ReportSendView.as_view, name="reports"),
+    # path("reports/", views.ReportSendView.as_view, name="reports"),
+    # path("reports/", views.ReportSendView.as_view, name="reports"),
+    # path("reports/", views.ReportSendView.as_view, name="reports"),
 ]
