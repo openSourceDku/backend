cdcd136b019a83e8b1c72fb67d4c2fd2172a8ddc
suhwan <jssunii951@gmail.com>
2025-07-08 00:03:20 +0900

    비품url오타 수정 및 add fixture modify send_test.py

--- apps/managers/urls.py
+++ apps/managers/urls.py
@@ -12,5 +12,5 @@ urlpatterns = [
     path('fixtures', FixtureCreateView.as_view(), name='fixture-create'),
     path('fixtures/{itemId}', FixtureUpdateView.as_view(), name='fixture-undate'),
     path('fixtures/{itemId}', FixtureDeleteView.as_view(), name='fixture-delete'),
-    path('fixtures/{itemId}', FixtureListView.as_view(), name='fixture-list'),
+    path('fixtures', FixtureListView.as_view(), name='fixture-list'),
 ]
