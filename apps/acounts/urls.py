015dc03408d4af21ef07046571eed58a007cebcd
maruiNia <maruinano@gmail.com>
2025-07-08 23:54:19 +0900

    managers 수정

--- apps/acounts/urls.py
+++ apps/acounts/urls.py
@@ -6,7 +6,7 @@ from rest_framework_simplejwt.views import TokenRefreshView
 app_name = "acounts"
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),                     #계정 등록
-    path('login/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'), 
-    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('logout/', LogoutView.as_view(), name='logout'),
+    path('login/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),  #로그인
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),             #토큰리프레쉬
+    path('logout/', LogoutView.as_view(), name='logout'),                           #로그아웃
 ]
--- apps/managers/urls.py
+++ apps/managers/urls.py
@@ -10,10 +10,13 @@ from .views import FixtureCreateView, FixtureUpdateView, FixtureDeleteView, Fixt
 urlpatterns = [
     path('', views.index, name = 'index'),
     path('fixtures', FixtureCreateView.as_view(), name='fixture-create'),
-    path('fixtures/{itemId}', FixtureUpdateView.as_view(), name='fixture-undate'),
-    path('fixtures/{itemId}', FixtureDeleteView.as_view(), name='fixture-delete'),
+    path('fixtures/<int:itemId>', FixtureUpdateView.as_view(), name='fixture-undate'),
+    path('fixtures/<int:itemId>', FixtureDeleteView.as_view(), name='fixture-delete'),
     path('fixtures', FixtureListView.as_view(), name='fixture-list'),
     path('teachers/', views.TeacherAPIView.as_view(), name='teachers_api'),
+    # path('classes/classrooms', ),
+    # path('classes/', ),
+    # path('/class-list/<int:classId>',)
     # path('teachers/classes/<int:class_id>', views.GetClassStudentsView.as_view(), name='get_class_students'),
     # path('teachers/classes/', views.TeacherClassListView.as_view(), name='classes'),
 ]
